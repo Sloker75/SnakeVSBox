dcc1e3035501d5e9746f0c5c9f89085d9a26f9cc
Sloker75 <79275738+Sloker75@users.noreply.github.com>
2022-11-28 18:17:26 -0600

    Snake movement
    The movement of the snake, as well as the camera behind them

--- Assets/Scripts/Snake/Snake.cs
+++ Assets/Scripts/Snake/Snake.cs
@@ -6,17 +6,38 @@ public class Snake : MonoBehaviour
 {
     [SerializeField] private SnakeHead _snakeHead;
     [SerializeField] private float _speed;
+    [SerializeField] private float _tailSpringiness;
 
+    private SnakeInput _snakeInput;
     private TailSpawner _tailSpawner;
+    private List<Segment> _tailSegments;
 
     private void Start()
     {
         _tailSpawner = GetComponent<TailSpawner>();
-        _tailSpawner.Generate();
+        _snakeInput = GetComponent<SnakeInput>();
+        _tailSegments = _tailSpawner.Generate();
     }
 
     private void FixedUpdate()
     {
-        _snakeHead.Move(_snakeHead.transform.position + _snakeHead.transform.up * (_speed * Time.fixedDeltaTime));
+        Move(_snakeHead.transform.position + _snakeHead.transform.up * (_speed * Time.fixedDeltaTime));
+
+        _snakeHead.transform.up = _snakeInput.GetDeractionToClick(_snakeHead.transform.position);
+    }
+
+    private void Move(Vector3 nextPosition)
+    {
+        var previuosPosition = _snakeHead.transform.position;
+
+        foreach (var tailSegment in _tailSegments)
+        {
+            var tempPosition = tailSegment.transform.position;
+            tailSegment.transform.position = Vector2.Lerp(tailSegment.transform.position, 
+                previuosPosition, 
+                _tailSpringiness * Time.fixedDeltaTime);
+            previuosPosition = tempPosition;
+        }
+        _snakeHead.Move(nextPosition);
     }
 }
